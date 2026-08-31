88dd391f39951abbdf9e07dec8d6aa4abf08af80
saifeliteqs-bit <saifeliteqs@gmail.com>
2026-08-31 17:24:49 +0400

    Update App.jsx

--- zbm-contracting/src/App.jsx
+++ zbm-contracting/src/App.jsx
@@ -10,6 +10,7 @@ import Stats from './sections/Stats';
 import Services from './sections/Services';
 import WhyChoose from './sections/WhyChoose';
 import Projects from './sections/Projects';
+import ProjectDetail from './sections/ProjectDetail';
 import Process from './sections/Process';
 import CTABanner from './sections/CTABanner';
 import Contact from './sections/Contact';
@@ -33,6 +34,7 @@ export default function App() {
           <Services />
           <WhyChoose />
           <Projects />
+          <ProjectDetail />
           <Process />
           <CTABanner />
           <Contact />
