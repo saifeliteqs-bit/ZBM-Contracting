38814a3cf510526defb9101496378a0f577e2bd1
saifeliteqs-bit <saifeliteqs@gmail.com>
2026-09-05 15:03:46 +0400

    Update App.jsx

--- zbm-contracting/src/App.jsx
+++ zbm-contracting/src/App.jsx
@@ -8,6 +8,7 @@ import FloatingButtons from './components/FloatingButtons';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ProjectsPage from './pages/ProjectsPage';
+import ContactPage from './pages/ContactPage';
 
 import './styles/global.scss';
 
@@ -26,6 +27,7 @@ export default function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
           </Routes>
         </main>
 
