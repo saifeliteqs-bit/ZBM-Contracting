41dfe486505ba2d1e0f63c0d144e2a309fe449d0
saifeliteqs-bit <saifeliteqs@gmail.com>
2026-09-05 14:46:16 +0400

    Update main.jsx

--- zbm-contracting/src/main.jsx
+++ zbm-contracting/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { LanguageProvider } from './hooks/useLanguage.jsx';
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <LanguageProvider>
-    <App />
-  </LanguageProvider>
+  <BrowserRouter>
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
+  </BrowserRouter>
 );
